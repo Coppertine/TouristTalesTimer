eab4f858b75a7f780edc212d2160e4428f865b2d
Coppertine <nick_bishop1@outlook.com>
2019-08-08 11:01:47 +1000

    Fixed Timer class to standards

--- src/com/coppertine/tafe/touristtales/Timer.java
+++ src/com/coppertine/tafe/touristtales/Timer.java
@@ -14,8 +14,94 @@ import java.time.LocalDateTime;
 
 enum TimerStatus { Running, Stopped };
 
+/**
+ *
+ * @author Coppertine
+ */
 public class Timer {
+    /**
+     *
+     */
     private TimerStatus status;
+    /**
+     *
+     */
     private LocalDateTime loggedStartTime;
+    /**
+     *
+     */
     private LocalDateTime loggedEndTime;
+
+
+
+    /**
+     * Empty Constructor.
+     */
+    public Timer() {
+    }
+
+    /**
+     * Constructor of the Timer object.
+     * @param timerStatus TimerStatus enumerator.
+     * @param loggedStart Start LocalDateTime.
+     * @param loggedEnd End LocalDateTime.
+     */
+    public Timer(
+            final TimerStatus timerStatus,
+            final LocalDateTime loggedStart,
+            final LocalDateTime loggedEnd) {
+        this.status = timerStatus;
+        this.loggedStartTime = loggedStart;
+        this.loggedEndTime = loggedEnd;
+    }
+
+    /**
+     *
+     * @return TimerStatus enumerator
+     * @see TimerStatus
+     */
+    public final TimerStatus getStatus() {
+        return status;
+    }
+
+    /**
+     *
+     * @param timerStatus TimerStatus enumerator
+     */
+    public final void setStatus(final TimerStatus timerStatus) {
+        this.status = timerStatus;
+    }
+
+    /**
+     *
+     * @return LocalDateTimer Start DateTime.
+     */
+    public final LocalDateTime getLoggedStartTime() {
+        return loggedStartTime;
+    }
+
+    /**
+     * sets Logged Start Time.
+     * @param loggedStart LocalDateTime object.
+     */
+    public final void setLoggedStartTime(final LocalDateTime loggedStart) {
+        this.loggedStartTime = loggedStart;
+    }
+
+    /**
+     *
+     * @return LocalDateTime
+     */
+    public final LocalDateTime getLoggedEndTime() {
+        return loggedEndTime;
+    }
+
+    /**
+     * Sets the Log End Time.
+     * @param loggedEnd LocalEndTime
+     */
+    public final void setLoggedEndTime(final LocalDateTime loggedEnd) {
+        this.loggedEndTime = loggedEnd;
+    }
+
 }
